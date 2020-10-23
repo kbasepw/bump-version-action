63891ab18905b537faf9f669526fd284e216e163
Carlos Prado <carlos.prado@cpradog.com>
2020-10-23 21:26:39 +0200

    fix: wait until action completes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const action = require('./action');
 const core = require('@actions/core');
 
-try {
-  const version_tag_prefix = core.getInput('version_tag_prefix', {required: false});
+(async function() {
+  try {
+    const version_tag_prefix = core.getInput('version_tag_prefix', {required: false});
 
-  const next_version = action(version_tag_prefix, core);
+    const next_version = await action(version_tag_prefix, core);
 
-  core.setOutput('version', next_version);
-} catch (error) {
-  core.setFailed(error.message);
-}
+    core.setOutput('version', next_version);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
